4be5bfd6cefb407500cd4f528d158d7870c3864c
WillB <williambatistapereira@hotmail.com>
2020-09-13 11:55:41 -0300

    projeto criado com springboot, criado Category in memormy

---
+++ src/main/java/com/devsuperior/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,32 @@
+package com.devsuperior.myfirstproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.myfirstproject.entities.Category;
+import com.devsuperior.myfirstproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfirstprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryrepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryrepository.save(cat1);
+		categoryrepository.save(cat2);
+		
+	}
+
+	
+}
---
+++ src/main/java/com/devsuperior/myfirstproject/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package com.devsuperior.myfirstproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.myfirstproject.entities.Category;
+import com.devsuperior.myfirstproject.repositories.CategoryRepository;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@Autowired  //resolve dependência de estâncias por baixo dos panos.
+	private CategoryRepository categoryrepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = categoryrepository.findAll();
+	    return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category cat = categoryrepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
+	
+}
